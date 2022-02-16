7609dca9fefb92361ed46ad0e6150fcfc397be52
dguru343 <kannanguru1999@gmail.com>
2022-02-16 16:20:39 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "Angular " + VERSION.major;
+  name = " bro" ;
   isShow = false;
 
   toggleStatus() {
